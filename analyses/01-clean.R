4588e2cf881f4b48717daca123b6cc824143d658
Andree Valle Campos <avallecam@gmail.com>
2025-04-23 21:55:22 -0400

    use link to keep original data in one place

--- analyses/01-clean.R
+++ analyses/01-clean.R
@@ -3,10 +3,11 @@ library(readxl)
 library(tidyverse)
 
 # Read raw data
-dat <- readxl::read_xlsx("data/linelist_20140701.xlsx")
+dat <- rio::import("https://github.com/reconhub/learn/raw/master/static/data/linelist_20140701.xlsx")
 
 # Clean raw data
 dat_clean <- dat %>% 
+  as_tibble() %>% 
   select(case_id,date_of_onset,date_of_outcome,outcome) %>% 
   mutate(across(.cols = c(date_of_onset,date_of_outcome),
                 .fns = as.Date)) %>% 
@@ -14,4 +15,4 @@ dat_clean <- dat %>%
 
 # Write clean data
 dat_clean %>% 
-  write_rds("output/linelist_clean.rds")
+  write_rds("outputs/linelist_clean.rds")
